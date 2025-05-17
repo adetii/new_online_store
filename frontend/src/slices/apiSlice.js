aa80f75e63de5634b9a0a533de6cf92855f16151
Adeti Francis Elorm <150345054+adetii@users.noreply.github.com>
2025-05-17 19:38:48 +0000

    Update apiSlice.js

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -2,7 +2,7 @@ import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 // Use environment variables to handle different environments
 const baseUrl = process.env.NODE_ENV === 'production'
-  ? '/api' // In production, prepend "/api" so requests go to `https://shopname.onrender.com/api/…`
+  ? '' // In production, prepend "/api" so requests go to `https://shopname.onrender.com/api/…`
   : '';    // In development, leave it empty so fetch('/api/…') proxies to your local server
 
 const baseQuery = fetchBaseQuery({
